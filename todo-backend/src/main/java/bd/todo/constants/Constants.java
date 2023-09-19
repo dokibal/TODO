25c86ae94ad16239a7ffa9f5e6a87ac9ff8272f2
dokibal <doktor.balazs1@gmail.com>
2023-09-19 08:10:15 +0200

    Move backend to another repo
    -For the easier handling of frontend and backend the backend part
    is moved to another repository: https://github.com/dokibal/todo-backend

--- todo-backend/src/main/java/bd/todo/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package bd.todo.constants;
-
-public class Constants {
-	public static final String TODO_TABLE_NAME = "todos";
-	public static final String USER_TABLE_NAME = "users";
-	public static final String USER_NAME_COLUMN = "user_name";
-}
