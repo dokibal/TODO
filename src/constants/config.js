95d5a3f2ee838a3dd5623351402d436e6b550504
dokibal <doktor.balazs1@gmail.com>
2023-09-25 08:35:48 +0200

    Change backend url back to heroku

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL = "http://localhost:8080/api/v1";
-// export const API_BASE_URL = "https://stormy-bayou-81080-2fa3e76b4bcc.herokuapp.com/api/v1";
+export const API_BASE_URL = "https://stormy-bayou-81080-2fa3e76b4bcc.herokuapp.com/api/v1";
